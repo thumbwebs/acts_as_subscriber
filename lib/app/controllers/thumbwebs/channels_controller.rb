c7293afe507d79557d137f1c9a7d0545ad00f2cb
arkietv <arkietv@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2009-01-13 13:38:28 +0000

    added channels and works controllers to client
    git-svn-id: http://tools.assembla.com/svn/railscoder/videoserver/branches/subscriber/vendor/plugins/thumbwebs@1443 f5e22e0a-0f59-dc11-ae45-00188b745eb1

---
+++ lib/app/controllers/thumbwebs/channels_controller.rb
@@ -0,0 +1,87 @@
+class Thumbwebs::ChannelsController < ApplicationController
+  # GET /channels
+  # GET /channels.xml
+  def index
+    @channels = Thumbwebs::Channel.find(:all, :params=>{:thumbwebs_id => 1})
+
+    respond_to do |format|
+      format.html {render :template => '/thumbwebs/channels/index'}
+      format.xml  { render :xml => @channels }
+    end
+  end
+
+  # GET /chatters/1
+  # GET /chatters/1.xml
+  def show
+    @channel = Thumbwebs::Channel.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @channel }
+    end
+  end
+
+  # GET /chatters/new
+  # GET /chatters/new.xml
+  def new
+    @chatter = Chatter.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @chatter }
+    end
+  end
+
+  # GET /chatters/1/edit
+  def edit
+    @chatter = Thumbwebs::Channel.find(params[:id])
+  end
+
+  # POST /chatters
+  # POST /chatters.xml
+  def create
+    @chatter = Thumbwebs::Channel.new(params[:chatter])
+    @chatter.user_id = logged_in_user.id
+    @chatter.service = "web"
+    @chatter.channel_id = params['channel_id']    
+    respond_to do |format|
+      if @channel.save
+        flash[:notice] = 'Channel was successfully created.'
+        format.html { redirect_to(channel_path(:id =>@channel.id)) }
+        format.xml  { render :xml => @chatter, :status => :created, :location => @chatter }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @chatter.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /chatters/1
+  # PUT /chatters/1.xml
+  def update
+    @chatter = Chatter.find(params[:id])
+
+    respond_to do |format|
+      if @chatter.update_attributes(params[:chatter])
+        flash[:notice] = 'Chatter was successfully updated.'
+        format.html { redirect_to(@chatter) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @chatter.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /chatters/1
+  # DELETE /chatters/1.xml
+  def destroy
+    @chatter = Chatter.find(params[:id])
+    @chatter.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(channel_chatters_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
