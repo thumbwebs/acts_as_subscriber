0d3435f812a3ca354652af3f85be80b84373a372
arkietv <arkietv@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2009-01-27 15:04:31 +0000

    PLUGIN if config.cache_classes is false in development environment then we get reload errors.  Do not think this is our fault.  It seems to fail with restful authentication. could be something in thumbwebs.rb
    git-svn-id: http://tools.assembla.com/svn/railscoder/videoserver/branches/subscriber/vendor/plugins/thumbwebs@1486 f5e22e0a-0f59-dc11-ae45-00188b745eb1

--- lib/app/models/thumbwebs/article.rb
+++ lib/app/models/thumbwebs/article.rb
@@ -1,5 +1,5 @@
 class Thumbwebs::Article  < ActiveResource::Base 
- 
+
   # In the case where you already have an existing model with the same name as the desired 
   # RESTful resource you can set the element_name value
   # self.element_name = "subscriber"
