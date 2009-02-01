819aec7e316038525aa3ab62631ab25f1153aba8
arkietv <arkietv@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2009-02-01 03:09:30 +0000

    PLUGIN added copy routine to install to copy public directory files to main rails app
    git-svn-id: http://tools.assembla.com/svn/railscoder/videoserver/branches/subscriber/vendor/plugins/thumbwebs@1524 f5e22e0a-0f59-dc11-ae45-00188b745eb1

--- install.rb
+++ install.rb
@@ -3,4 +3,10 @@ require 'fileutils'
 
 thumbwebs_config = File.dirname(__FILE__) + '/../../../config/thumbwebs.yml'
 FileUtils.cp File.dirname(__FILE__) + '/thumbwebs.yml.tpl', thumbwebs_config unless File.exist?(thumbwebs_config)
+
+### copy public files directory to main Rails app
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/thumbwebs                                                                                                                                                                                                                                                                                                                              '
+FileUtils.cp_r File.dirname(__FILE__) + '/public_files', thumbwebs_public unless File.exist?(thumbwebs_config)
+
+### display readme file when installation finishes
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
