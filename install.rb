a0a8eed41186344caa4065cc7acc40e19b485eb3
arkietv <arkietv@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2009-02-02 03:51:54 +0000

    PLUGIN modified install to fix syntax errors and to create directories under public/images for swf files
    git-svn-id: http://tools.assembla.com/svn/railscoder/videoserver/branches/subscriber/vendor/plugins/thumbwebs@1531 f5e22e0a-0f59-dc11-ae45-00188b745eb1

--- install.rb
+++ install.rb
@@ -7,19 +7,21 @@ FileUtils.cp File.dirname(__FILE__) + '/thumbwebs.yml.tpl', thumbwebs_config unl
 
 puts "copying thumbwebs images"
 ### copy public image files directory to main Rails app
-thumbwebs_public = File.dirname(__FILE__) + '/../../../public/images/thumbwebs                                                                                                                                                                                                                                                                                                                              '
-FileUtils.cp_r File.dirname(__FILE__) + '/public_files/images', thumbwebs_public unless File.exist?(thumbwebs_config)
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/images/thumbweb/s                                                                                                                                                                                                                                                                                                                              '
+FileUtils.cp_r File.dirname(__FILE__) + '/public_files/images/', thumbwebs_public unless File.exist?(thumbwebs_config)
 
 puts "copying thumbwebs javascripts"
 ### copy public javascripts files directory to main Rails app
-thumbwebs_public = File.dirname(__FILE__) + '/../../../public/javascripts/thumbwebs                                                                                                                                                                                                                                                                                                                              '
-FileUtils.cp_r File.dirname(__FILE__) + '/public_files/javascripts', thumbwebs_public unless File.exist?(thumbwebs_config)
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/javascripts/thumbwebs/'
+thumbwebs_plugin= File.dirname(__FILE__) + '/public_files/javascripts/'
+FileUtils.cp_r thumbwebs_plugin, thumbwebs_public unless File.exist?(thumbwebs_public)
+puts "thumbwebs_public = #{thumbwebs_public}"
 
 puts "copying thumbwebs flash files"
 ### copy public swf files directory to main Rails app
-thumbwebs_public = File.dirname(__FILE__) + '/../../../public/swf/thumbwebs                                                                                                                                                                                                                                                                                                                              '
-FileUtils.cp_r File.dirname(__FILE__) + '/public_files/swf', thumbwebs_public unless File.exist?(thumbwebs_config)
-
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/images/thumbwebs/' 
+FileUtils.cp_r File.dirname(__FILE__) + '/public_files/swf/', thumbwebs_public #unless File.exist?(thumbwebs_public)
+puts "copying"
 
 ### display readme file when installation finishes
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
--- thumbwebs.yml.tpl
+++ thumbwebs.yml.tpl
@@ -3,20 +3,28 @@ development:
   username: jimmy
   password: homerun99
   api: '94539034590'
-  site_url: 'http://thumbwebs.com/'
+  site_url: 'http://thumbwebs.com'
   authorized_user: 'jimmy'
   articles_enabled: true
+  works_enabled: true
   
 production:
-  channel_id: 1
-  username: rob
-  password: orange99
-  api: '2345234553245'
-  site_url: 'http://localhost:3000/'
+  channel_id: 3
+  username: jimmy
+  password: homerun99
+  api: '94539034590'
+  site_url: 'http://thumbwebs.com'
+  authorized_user: 'jimmy'
+  articles_enabled: true
+  works_enabled: true
     
 test:
-  channel_id: 1
-  username: rob
-  password: orange99
-  api: '092459034509'
-  site_url: 'http://localhost:3000'
+  channel_id: 3
+  username: jimmy
+  password: homerun99
+  api: '94539034590'
+  site_url: 'http://thumbwebs.com'
+  authorized_user: 'jimmy'
+  articles_enabled: true
+  works_enabled: true
+    
