3bef97a5d3bb6d3373b52f28560760dc401abf6e
arkietv <arkietv@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2009-02-02 03:04:47 +0000

    PLUGIN copying images, javascripts, swf files from plugin to client in install
    git-svn-id: http://tools.assembla.com/svn/railscoder/videoserver/branches/subscriber/vendor/plugins/thumbwebs@1530 f5e22e0a-0f59-dc11-ae45-00188b745eb1

--- install.rb
+++ install.rb
@@ -1,12 +1,25 @@
 # Install hook code here
 require 'fileutils'
 
+puts "copying thumbwebs.yml"
 thumbwebs_config = File.dirname(__FILE__) + '/../../../config/thumbwebs.yml'
 FileUtils.cp File.dirname(__FILE__) + '/thumbwebs.yml.tpl', thumbwebs_config unless File.exist?(thumbwebs_config)
 
-### copy public files directory to main Rails app
-thumbwebs_public = File.dirname(__FILE__) + '/../../../public/thumbwebs                                                                                                                                                                                                                                                                                                                              '
-FileUtils.cp_r File.dirname(__FILE__) + '/public_files', thumbwebs_public unless File.exist?(thumbwebs_config)
+puts "copying thumbwebs images"
+### copy public image files directory to main Rails app
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/images/thumbwebs                                                                                                                                                                                                                                                                                                                              '
+FileUtils.cp_r File.dirname(__FILE__) + '/public_files/images', thumbwebs_public unless File.exist?(thumbwebs_config)
+
+puts "copying thumbwebs javascripts"
+### copy public javascripts files directory to main Rails app
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/javascripts/thumbwebs                                                                                                                                                                                                                                                                                                                              '
+FileUtils.cp_r File.dirname(__FILE__) + '/public_files/javascripts', thumbwebs_public unless File.exist?(thumbwebs_config)
+
+puts "copying thumbwebs flash files"
+### copy public swf files directory to main Rails app
+thumbwebs_public = File.dirname(__FILE__) + '/../../../public/swf/thumbwebs                                                                                                                                                                                                                                                                                                                              '
+FileUtils.cp_r File.dirname(__FILE__) + '/public_files/swf', thumbwebs_public unless File.exist?(thumbwebs_config)
+
 
 ### display readme file when installation finishes
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
